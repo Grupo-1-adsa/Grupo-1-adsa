f01f933c91dcf2ab0f1adfb18871417032ae3964
fernandosantos1 <fernando.santos@bandtec.com.br>
2021-08-30 18:25:46 -0300

    sobre nos responsividade

--- Site/main.js
+++ Site/main.js
@@ -1,18 +1,25 @@
+// import nav_fundo from './img/testeroxo.svg';
+
 let contador = 0;
 let nav_bar = document.getElementById("nav_bar");
-let body = document.getElementById("body");
+// let fundo = document.getElementById("retRoxo1")
 
  const nav_button =()=> {
     if (contador === 0) {
             
         contador++
-        nav_bar.style.opacity = "1"
-        body.style.overflow = "hidden"
+        // nav_bar.style.opacity = "1"
+        // nav_fundo.style.opacity="1"
+        // fundo.style.opacity="1"
+        nav_bar.style.backgroundColor = "var(--cor-primario-azul)"
 
     } else if (contador === 1) {
 
         contador--
-        nav_bar.style.opacity = "0.60"
+        // nav_bar.style.opacity = "0.60"
+        // nav_fundo.style.opacity="0.60"
+        // fundo.style.opacity="0.60"
+        nav_bar.style.backgroundColor = ""
 
     }
    
