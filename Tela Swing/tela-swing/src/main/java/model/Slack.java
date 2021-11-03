124e7ef69ca0d13aba6a54c810399ee79edc84a3
Fernando Marques dos Santos <fernando.santos@bandtec.com.br>
2021-11-03 18:23:15 -0300

    slack

--- Tela Swing/tela-swing/src/main/java/model/Slack.java
+++ Tela Swing/tela-swing/src/main/java/model/Slack.java
@@ -1,11 +1,38 @@
-package com.mycompany.looca2.model;
+package model;
 
-import net.minidev.json.JSONObject;
+import com.nimbusds.jose.shaded.json.JSONObject;
 
+import java.io.IOException;
 import java.net.http.HttpClient;
+import java.net.http.HttpResponse;
+import java.net.URI;
+import java.net.http.HttpRequest;
+
 
 public class Slack {
+
     private static final HttpClient client = HttpClient.newHttpClient();
-    private static final String URL = "https://hooks.slack.com/services/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";
-    
+    private static final String URL = "https://hooks.slack.com/services/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";
+
+    public static void sendMessage(JSONObject content) throws IOException, InterruptedException {
+
+        HttpRequest request = HttpRequest.newBuilder(
+                        URI.create(URL))
+                .header("accept", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(content.toString()))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(String.format("Status: %s", response.statusCode()));
+        System.out.println(String.format("Response: %s", response.body()));
+    }
+    public static void main( String Args[] ) throws IOException, InterruptedException {
+
+        JSONObject json = new JSONObject();
+
+        json.put("text", "inativo? :shrug: :Smile:");
+
+        Slack.sendMessage(json);
+    }
 }
