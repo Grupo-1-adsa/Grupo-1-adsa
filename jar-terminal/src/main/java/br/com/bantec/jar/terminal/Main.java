1d7e12a6dc3c99b1b93acb24def5f3b6869af73f
LeoIannotta <leonardo-iannotta@bandtec.com.br>
2021-11-22 00:34:21 -0300

    jar para terminal

---
+++ jar-terminal/src/main/java/br/com/bantec/jar/terminal/Main.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.bantec.jar.terminal;
+
+import controller.EquipamentoDAO;
+import controller.FuncionarioDAO;
+import controller.LeituraDAO;
+import java.io.Console;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
+import logging.Log;
+import model.Slack;
+
+/**
+ *
+ * @author leoia
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("+-------------------------------------------------------------------------------------------+");
+        System.out.println("| __________                        ____   ____.__            .___                          |\n"
+                + "| \\______   \\ ____   _____          \\   \\ /   /|__| ____    __| _/____                      |\n"
+                + "|  |    |  _// __ \\ /     \\   ______ \\   Y   / |  |/    \\  / __ |/  _ \\                     |\n"
+                + "|  |    |   \\  ___/|  Y Y  \\ /_____/  \\     /  |  |   |  \\/ /_/ (  <_> )                    |\n"
+                + "|  |______  /\\___  >__|_|  /           \\___/   |__|___|  /\\____ |\\____/                     |\n"
+                + "|         \\/     \\/      \\/                            \\/      \\/                           |\n"
+                + "|                                                                                           |\n"
+                + "|    _____    ____                                                                          |\n"
+                + "|    \\__  \\  /  _ \\                                                                         |\n"
+                + "|     / __ \\(  <_> )                                                                        |\n"
+                + "|    (____  /\\____/                                                                         |\n"
+                + "|         \\/                                                                                |\n"
+                + "| __________                                       __  .__               __                 |\n|"
+                + "| \\______   \\_______  ____   ____ _____    _______/  |_|__| ____ _____ _/  |_  ___________ |\n"
+                + "|  |     ___/\\_  __ \\/  _ \\_/ ___\\\\__  \\  /  ___/\\   __\\  |/    \\\\__  \\\\   __\\/  _ \\_  __ \\ |\n"
+                + "|  |    |     |  | \\(  <_> )  \\___ / __ \\_\\___ \\  |  | |  |   |  \\/ __ \\|  | (  <_> )  | \\/ |\n"
+                + "|  |____|     |__|   \\____/ \\___  >____  /____  > |__| |__|___|  (____  /__|  \\____/|__|    |\n"
+                + "|                               \\/     \\/     \\/               \\/     \\/                    |");
+        System.out.println("+-------------------------------------------------------------------------------------------+");
+
+        FuncionarioDAO funcionarioDAO = new FuncionarioDAO();
+        EquipamentoDAO equipamentoDAO = new EquipamentoDAO();
+        LeituraDAO leituraDAO = new LeituraDAO();
+        Slack slack = new Slack();
+        Log log = new Log();
+        Scanner scan = new Scanner(System.in);
+        Console console = System.console();
+        
+        do {
+            System.out.println("Entre com o username:");
+            String login = scan.next();
+            System.out.println("Entre com a senha:");
+            String senha = scan.next();
+            funcionarioDAO.loginBanco(login, senha);
+
+            if (funcionarioDAO.verificaLogin() == true) {
+                funcionarioDAO.loginBanco(login, senha);
+                slack.enviaMensagem(funcionarioDAO.getNomeFunc());
+                equipamentoDAO.enviarEquipamento(funcionarioDAO.getFkFuncionario());
+                equipamentoDAO.selectEquipamento(funcionarioDAO.getFkFuncionario());
+                System.out.println("\n(!) Login realizado com sucesso (!)\n");
+                Timer timer = new Timer();
+                final long segundos = 10000;
+                TimerTask tarefa = new TimerTask() {
+                    @Override
+                    public void run() {
+
+                        leituraDAO.enviarLeitura(equipamentoDAO.getFkLeitura());
+
+                    }
+                };
+                timer.scheduleAtFixedRate(tarefa, 10, segundos);
+
+            } else {
+                System.out.println("Login ou senha incorretos.");
+            }
+        } while (!funcionarioDAO.verificaLogin() == true);
+
+    }
+}
---
+++ jar-terminal/src/main/java/model/Slack.java
@@ -0,0 +1,70 @@
+package model;
+
+import com.github.britooo.looca.api.group.memoria.Memoria;
+import com.github.britooo.looca.api.group.processador.Processador;
+import com.nimbusds.jose.shaded.json.JSONObject;
+import org.springframework.dao.EmptyResultDataAccessException;
+import logging.Log;
+
+import java.io.IOException;
+import java.net.http.HttpClient;
+import java.net.http.HttpResponse;
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+public class Slack {
+
+    private static final HttpClient client = HttpClient.newHttpClient();
+    private static final String URL = "https://hooks.slack.com/services/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";
+
+    public static void sendMessage(JSONObject content) throws IOException, InterruptedException {
+        Log log = new Log();
+        try {
+            HttpRequest request = HttpRequest.newBuilder(
+                            URI.create(URL))
+                    .header("accept", "application/json")
+                    .POST(HttpRequest.BodyPublishers.ofString(content.toString()))
+                    .build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            System.out.println(String.format("Status: %s", response.statusCode()));
+            System.out.println(String.format("Response: %s", response.body()));
+        } catch (EmptyResultDataAccessException e) {
+            System.out.println("Não enviou a mensagem do Slack");
+            log.slackLog();
+        }
+    }
+
+    public void enviaMensagem(String nomeFunc) throws IOException, InterruptedException {
+        Log log = new Log();
+        Processador processador = new Processador();
+        Memoria memoria = new Memoria();
+        JSONObject json = new JSONObject();
+        Timer timer = new Timer();
+        final long segundos = 600000;
+        TimerTask tarefa = new TimerTask() {
+            @Override
+            public void run() {
+
+                if (memoria.getEmUso() / memoria.getTotal() <= 0.4 && processador.getUso() <= 30.0) {
+                    json.put("text", "O @" + nomeFunc + " pode estar inativo pode estar inativo é melhor verificar :male-detective:");
+                    try {
+                        sendMessage(json);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } 
+                }
+
+            }
+        };
+        timer.scheduleAtFixedRate(tarefa, 10, segundos);
+
+
+    }
+}
